e333a93fcb34b2e5521e032637568856db8d7dbe
abdullah Abdulkareem <abdullah>
2024-01-24 14:46:04 -0600

    Initial Commit

---
+++ DreamFiT_V2/DreamFiT_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  DreamFiT_V2App.swift
+//  DreamFiT_V2
+//
+//  Created by Abdullah Abdulkareem on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct DreamFiT_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
